f020a5f04316cd9062aea0480ef2f521c41fa0d3
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-11-12 03:48:16 -0500

    feat: export motion

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+export * from 'framer-motion'
 export * from './stories'
--- stories/molecules/Acordion/index.jsx
+++ stories/molecules/Acordion/index.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
-import React, {
+import {
   useEffect,
   useRef,
   useState
