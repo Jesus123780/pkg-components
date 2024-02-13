a9f4157be70266ba9251c346976bb8376277ca2b
Cristian <cristianjuvinao801@gmai.com>
2024-02-13 08:30:12 -0500

    feat: publish action 3

--- stories/organisms/LateralStoreInfo/LateralStoreInfo.stories.tsx
+++ stories/organisms/LateralStoreInfo/LateralStoreInfo.stories.tsx
@@ -1,34 +1,57 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react'
 import { LateralStoreInfo } from './index'
+import { Text } from '../../atoms'
 
-const meta: Meta<typeof LateralStoreInfo> = {
-    component: LateralStoreInfo,
-    title: "atoms/LateralStoreInfo",
-    parameters: {
-      controls: { expanded: true },
-    },
-    args: {
-      show: true,
-      active: 1,
-      handleClose: () => {
-        console.log('Close button clicked')
-      }
-    },
-};
-
-export default meta;
-
-type Story = StoryObj<typeof LateralStoreInfo>;
-
-export const Default: Story = {
-  parameters: {
+export default {
+  title: 'organisms/LateralStoreInfo',
+  component: LateralStoreInfo,
+  argTypes: {
+    backgroundColor: { control: 'color' }
   },
   args: {
+    active: 0,
+    fState: 0,
+    idStore: '',
+    minOrder: 0,
+    overActive: 0,
+    rating: 0,
     show: true,
-    active: 1,
-    handleClose: () => {
-      console.log('Close button clicked')
-    }
-  },
-};
+    steps: [],
+    schedulesStore: [],
+    setRatingStar: (props: any) => console.log(props),
+    handleClose: () => console.log('handleClose'),
+    setActive: (active: any) => console.log(active),
+    setRating: (rating: any) => console.log(rating),
+    handleOverActive: (index: any) => console.log(index),
+    removeFav: (idStore: any, fState: any) => console.log(idStore, fState),
+    addFav: (idStore: any) => console.log(idStore)
+  
+  }
+}
 
+const Template = (args) => {
+  return <>
+  <Text>Hola</Text>
+  <LateralStoreInfo {...args} />
+  </>
+}
+
+export const TemplateLateralStoreInfo = Template.bind({})
+TemplateLateralStoreInfo.args = {
+  active: 0,
+  fState: 0,
+  idStore: '',
+  minOrder: 0,
+  overActive: 0,
+  rating: 0,
+  show: true,
+  steps: [],
+  schedulesStore: [],
+  setRatingStar: (props: any) => console.log(props),
+  handleClose: () => console.log('handleClose'),
+  setActive: (active: any) => console.log(active),
+  setRating: (rating: any) => console.log(rating),
+  handleOverActive: (index: any) => console.log(index),
+  removeFav: (idStore: any, fState: any) => console.log(idStore, fState),
+  addFav: (idStore: any) => console.log(idStore)
+}
