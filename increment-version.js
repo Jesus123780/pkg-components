57da8ed7c854c133acdaf1869bf67dae55e09200
Cristian <cristianjuvinao801@gmai.com>
2024-02-16 01:57:35 -0500

    feat: update code

--- increment-version.js
+++ increment-version.js
@@ -6,19 +6,21 @@ const packageJson = JSON.parse(fs.readFileSync('package.json'));
 // Obtiene la versión actual y la divide en partes (ej. 1.0.0 => [1, 0, 0])
 const versionParts = packageJson.version.split('.').map(part => parseInt(part));
 
-// Incrementa la parte de la versión en 10
-versionParts[2] += 10;
+// Incrementa la parte menor de la versión en 10
+versionParts[2] += 1;
 
-// Si la parte menor de la versión es mayor o igual a 100, incrementa la parte media y reinicia la parte menor
+// Si la parte menor de la versión es mayor o igual a 100, incrementa la parte media
+// y reinicia la parte menor
 if (versionParts[2] >= 100) {
     versionParts[1]++;
-    versionParts[2] = 0;
+    versionParts[2] %= 100; // Reinicia la parte menor
 }
 
-// Si la parte media de la versión es mayor o igual a 10, incrementa la parte mayor y reinicia la parte media
+// Si la parte media de la versión es mayor o igual a 10, incrementa la parte mayor
+// y reinicia la parte media
 if (versionParts[1] >= 10) {
     versionParts[0]++;
-    versionParts[1] = 0;
+    versionParts[1] %= 10; // Reinicia la parte media
 }
 
 // Actualiza la versión en el package.json
