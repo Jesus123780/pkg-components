294a39e8fda76b438c2689976b1546bc2bc4c690
Cristian <cristianjuvinao801@gmai.com>
2024-02-15 22:03:06 -0500

    fix: eslint

---
+++ stories/molecules/AlertBox/types.ts
@@ -0,0 +1,14 @@
+export interface AlertContentProps {
+  err: {
+    color?: string;
+    duration?: number;
+    message: string;
+  };
+  closed: boolean;
+}
+
+export interface ContainerToastProps {
+  error: boolean;
+  closed: boolean;
+  color?: string;
+}
---
+++ stories/molecules/QuantityButton/types.ts
@@ -0,0 +1,17 @@
+export interface QuantityButtonProps {
+    border: any
+    classNameQuantity: string
+    disabled?: boolean
+    handleDecrement?: () => void
+    handleIncrement?: () => void
+    label?: string
+    margin: any
+    padding: any
+    quantity: number
+    showNegativeButton?: boolean
+    showPositiveButton?: boolean
+    validationOne?: any
+    validationZero?: boolean
+    width?: string
+  
+  }
