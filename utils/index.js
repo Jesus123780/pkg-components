f76e64b822b3bf3e5a5b6a32c139c2cd7a20a9d1
cristianbonny801 <tu-correo-de-github@example.com>
2025-10-26 12:30:41 -0500

    feat:update

--- utils/index.js
+++ utils/index.js
@@ -133,24 +133,3 @@ export const isEmail = email => {
 }
 
 export const passwordConfirm = (value, valueConfirm) => { return !(value === valueConfirm) }
-
-export const ROUTES = Object.freeze({
-  index: '/',
-  home: '/home',
-  register: '/register',
-  dashboard: '/dashboard',
-  profile: '/profile',
-  categories: '/categories',
-  configuration: '/configuration',
-  orders: '/orders',
-  products: '/products',
-  notFound: '/404',
-  serverError: '/500',
-  // NO LAYOUT
-  'verify-email': '/verify-email',
-  'forgot-password': '/forgot-password',
-  'reset-password': '/reset-password',
-  merchant: '/merchant',
-  login: '/login'
-
-})
